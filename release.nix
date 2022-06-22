6691066e13d6574db4a94d24271937981c09f59d
Stefan Hertrampf <stefan.hertrampf@cyberus-technology.de>
2022-06-22 15:32:49 +0200

    nix: add cross compile libs and apps
    We transparantly compile for our non-std environment and add the custom
    libc via nix. This for now needs redefinition of the compiler flags, but
    we might be able to parse those from the cmake file.

--- release.nix
+++ release.nix
@@ -6,9 +6,43 @@ let
 
   inherit (pkgs) cmake stdenv;
 
+  # TODO: Maybe its possible to parse those from the toolchain file?
+  cross-flags = pkgs.lib.concatStringsSep " " [
+  "-nostdinc"
+  "-mno-red-zone"
+  "-m64"
+  "-march=westmere"
+  "-mno-3dnow"
+  "-mno-mmx"
+  "-mno-sse"
+  ];
+
+  systemLib =  rec {
+    libc = stdenv.mkDerivation {
+      name = "liblibc";
+      nativeBuildInputs = [ cmake ];
+      src = ./libc;
+    };
+  };
+
+  crossCompileDrv = drv: drv.overrideAttrs (
+    old: {
+      preConfigure = ''
+        cmakeFlagsArray=(
+          $cmakeFlagsArray
+          "-DCMAKE_CXX_FLAGS=${cross-flags}"
+          "-DCMAKE_SYSTEM_NAME=x86_64_Baremetal"
+          )
+      '';
+      buildInputs = [ systemLib.libc ] ++ (builtins.map crossCompileDrv (old.buildInputs or []));
+    });
+
 in
 rec {
   recurseForDerivations = true;
+
+  inherit systemLib;
+
   lib = rec {
     recurseForDerivations = true;
     a = stdenv.mkDerivation {
@@ -33,10 +67,25 @@ rec {
       src = ./d;
     };
   };
+
+  # Native Linux app
   myapp = stdenv.mkDerivation {
     name = "myapp";
+
+    # Need to disable format security otherwise the compiler complains.
+    preConfigure = ''
+      cmakeFlagsArray=(
+        $cmakeFlagsArray
+        "-DCMAKE_CXX_FLAGS=-Wno-format-security"
+        )
+    '';
+
     buildInputs = with lib; [ a b c d ];
     nativeBuildInputs = [ cmake ];
     src = ./app;
   };
+
+  # Non-std libs and app with custom libc
+  lib-cross = pkgs.lib.mapAttrs (name: val: if (pkgs.lib.isDerivation val) then crossCompileDrv val else val) lib;
+  myapp-cross = crossCompileDrv myapp;
 }
